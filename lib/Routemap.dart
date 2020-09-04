6f14cc2c8da55e2f853a2981009a1835af29c26a
mashhoor58 <mashhoor.58@hotmail.com>
2020-09-04 18:24:43 +0300

    lll22

--- lib/Routemap.dart
+++ lib/Routemap.dart
@@ -10,7 +10,7 @@ class Routemap extends StatelessWidget {
         home:Scaffold(
         appBar: AppBar(
           centerTitle: true,
-          title: Text(' M'),//ffdkkk
+          title: Text(' M'),
         ),
 body: Center(
   child:Image(
